ea226142607d9e25b1592f9a9646a6db8260fd47
Rengo _ <renatovaldez6879@gmail.com>
2023-09-16 10:45:33 -0400

    Added sample question objects that appear when start quiz button clicked

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,20 @@
 var countTime = document.getElementById("start")
 var timerEl = document.getElementById("timer")
+var questionBlock = document.getElementById("questions")
+var choicesBlock = document.getElementById("choices")
+
+let questions = [
+    {
+    question:"Question 1",
+    choices: ["one","two","three","four"],
+    answer: "right"
+    
+    },{
+    question: "Question 2",
+    choices: ["right", "wrong", "wrong", "wrong"],
+    answer: "right"
+    }
+]
 
 
 function startGame() {
@@ -15,7 +30,22 @@ function startGame() {
     else{
     clearInterval(gameTimer);
     timerEl.textContent = "Time is up!";
-    gameOver();
+    gameOver(); 
     }
   }, 1000);
+
+    questionBlock.textContent = questions[0].question;
+    
+    for (var i=0; i < questions[0].choices.length; i++) {
+        choicesBlock.append(questions[0].choices[i]);
+    }
+    
+    var answer = document.querySelector("#choices");
+    
+    answer.addEventListener('click', function(event) {
+        if (event.target === questions[0].choices[3])
+        console.log("winner")
+    })
+        
 }
+
